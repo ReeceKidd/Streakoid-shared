4f0b667899494b9a6c5cae333102f4087f576ecd
Reece Kidd <reecekidd95@gmail.com>
2019-10-30 11:42:22 +0000

    feat: Updated index exports to include individual reducers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-import { rootReducer, AppState } from './reducers/index';
 import * as types from './actions/types';
+import { authReducer } from './reducers/authReducer';
+import { feedbackReducer } from './reducers/feedbackReducer';
+import { friendReducer } from './reducers/friendReducer';
+import { friendRequestReducer } from './reducers/friendRequestReducer';
+import { soloStreakReducer } from './reducers/soloStreakReducer';
+import { teamStreakReducer } from './reducers/teamStreakReducer';
+import { userReducer } from './reducers/userReducer';
 
-export { rootReducer, AppState, types };
+export {
+    types,
+    authReducer,
+    feedbackReducer,
+    friendReducer,
+    friendRequestReducer,
+    soloStreakReducer,
+    teamStreakReducer,
+    userReducer,
+};
