361d143576c995b9dc7cbe31e99391b171c7477d
Reece Kidd <reecekidd95@gmail.com>
2019-10-31 08:28:05 +0000

    feat: Updated getSharedActions imports

--- src/actions/getSharedActions.ts
+++ src/actions/getSharedActions.ts
@@ -3,9 +3,11 @@ import { Reducer } from 'redux';
 import { emailActions } from './emailActions';
 import { feedbackActions } from './feedbackActions';
 import { friendActions } from './friendActions';
-import { friendRequestActions, teamMemberStreakTaskActions, teamStreakActions } from '..';
+import { friendRequestActions } from './friendRequestActions';
 import { soloStreakActions } from './soloStreakActions';
 import { stripeActions } from './stripeActions';
+import { teamMemberStreakTaskActions } from './teamMemberStreakTaskActions';
+import { teamStreakActions } from './teamStreakActions';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const getSharedActions = (streakoid: typeof streakoidSDK, rootReducer: Reducer) => {
