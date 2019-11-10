a89c230d16217c34f12fc25b6c1587ca5fedad10
Reece Kidd <reecekidd95@gmail.com>
2019-11-10 16:11:20 +0000

    refactor: Changed from completeSoloStreaksReminder to completeStreaksReminder

--- src/reducers/userReducer.ts
+++ src/reducers/userReducer.ts
@@ -90,7 +90,7 @@ const initialState: UserReducerInitialState = {
             originalImageUrl: 'https://streakoid-profile-pictures.s3-eu-west-1.amazonaws.com/steve.jpg',
         },
         notifications: {
-            completeSoloStreaksReminder: {
+            completeStreaksReminder: {
                 pushNotification: false,
                 emailNotification: false,
                 reminderTime: 21,
