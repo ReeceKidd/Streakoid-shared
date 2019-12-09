2c405a675cc27855586de8431a0e6228d00f09af
Reece Kidd <reecekidd95@gmail.com>
2019-12-09 14:19:02 +0000

    chore: Removed console.log

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -132,11 +132,9 @@ const userActions = (streakoid: typeof streakoidSDK) => {
         try {
             dispatch({ type: UPDATE_CURRENT_USER_IS_LOADING });
             const updatedUser = await streakoid.user.updateCurrentUser({ updateData });
-            console.log('UPDATED USER SUCCESSFULLY');
             dispatch({ type: UPDATE_CURRENT_USER, user: updatedUser });
             dispatch({ type: UPDATE_CURRENT_USER_IS_LOADED });
         } catch (err) {
-            console.log(err.response.data);
             if (err.response) {
                 dispatch({ type: UPDATE_CURRENT_USER_FAIL, errorMessage: err.response.data.message });
             } else {
