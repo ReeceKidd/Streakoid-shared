afb0ab2d9d7783cd25fdef600b76bba55038d06a
Reece Kidd <reecekidd95@gmail.com>
2020-03-11 09:09:50 -0300

    feat: Upgraded getAverageStreak to solve infinity issue

--- src/helpers/streakCalculations/getAverageStreak.ts
+++ src/helpers/streakCalculations/getAverageStreak.ts
@@ -3,8 +3,9 @@ import { PastStreak, CurrentStreak } from '@streakoid/streakoid-sdk/lib';
 export const getAverageStreak = (currentStreak: CurrentStreak, pastStreaks: PastStreak[]) => {
     let pastStreakSum = 0;
     pastStreaks.map(pastStreak => (pastStreakSum += pastStreak.numberOfDaysInARow));
-    const totalStreakLength = pastStreakSum + currentStreak.numberOfDaysInARow;
+    const totalStreakSum = pastStreakSum + currentStreak.numberOfDaysInARow;
     // Plus one for the current streak
-    const averageStreak = totalStreakLength / (pastStreaks.length + 1);
+    if (totalStreakSum === 0) return 0;
+    const averageStreak = totalStreakSum / (pastStreaks.length + 1);
     return averageStreak;
 };
