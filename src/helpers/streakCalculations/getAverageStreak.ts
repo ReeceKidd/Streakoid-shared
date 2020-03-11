5383a687a4cfb9b5f22e17ff78ec9e06ed9098e1
Reece Kidd <reecekidd95@gmail.com>
2020-03-11 09:17:29 -0300

    feat: Added logging statements to getAverageStreak

--- src/helpers/streakCalculations/getAverageStreak.ts
+++ src/helpers/streakCalculations/getAverageStreak.ts
@@ -6,6 +6,10 @@ export const getAverageStreak = (currentStreak: CurrentStreak, pastStreaks: Past
     const totalStreakSum = pastStreakSum + currentStreak.numberOfDaysInARow;
     // Plus one for the current streak
     if (totalStreakSum === 0) return 0;
+    console.log('totalStreakSum', totalStreakSum);
+    console.log('pastStreaks length', pastStreaks.length);
+    console.log('pastStraeaks + 1', pastStreaks.length + 1);
     const averageStreak = totalStreakSum / (pastStreaks.length + 1);
+    console.log('averageStreak', averageStreak);
     return averageStreak;
 };
