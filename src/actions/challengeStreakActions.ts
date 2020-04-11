724eecff1e4c5b7891a4b8a804c76e0afad7bdd9
Reece Kidd <reecekidd95@gmail.com>
2020-04-11 15:22:01 +0100

    chore: Removed unnecessary p

--- src/actions/challengeStreakActions.ts
+++ src/actions/challengeStreakActions.ts
@@ -230,7 +230,7 @@ const challengeStreakActions = (streakoid: typeof streakoidSDK) => {
                 username: currentUser.username,
                 userProfileImage: currentUser.profileImages.originalImageUrl,
                 longestStreak: getLongestStreak(currentStreak, pastStreaks),
-                averageStreak: getAverageStreak(currentStreak, pastStpreaks),
+                averageStreak: getAverageStreak(currentStreak, pastStreaks),
                 totalTimesTracked: 0,
                 daysSinceStreakCreation: getDaysSinceStreakCreation({
                     createdAt: new Date(createdAt),
